bb90a736aa35173e24b1726302ceb84be4f7c877
matheus <matheusgalvain@gmail.com>
2024-09-12 22:43:36 -0300

    update admin controller

---
+++ admin/manage-users.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Admin | Gerenciamento de Usuários</title>
+    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
+    <link href="../css/styles.css" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="../js/scripts.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
+    <script src="../js/datatables-simple-demo.js"></script>
+</head>
+
+<body class="sb-nav-fixed">
+    <?php include_once('includes/navbar.php'); ?>
+
+    <div id="layoutSidenav">
+        <?php include_once('includes/sidebar.php'); ?>
+
+        <div id="layoutSidenav_content">
+            <main>
+                <div class="container-fluid px-4">
+                    <h1 class="mt-4">Gerenciamento de Usuários</h1>
+                    <ol class="breadcrumb mb-4">
+                        <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
+                        <li class="breadcrumb-item active">Gerenciamento de Usuários</li>
+                    </ol>
+                    <div class="card mb-4">
+                        <div class="card-header">
+                            <i class="fas fa-table me-1"></i>
+                            Detalhes dos Usuários
+                        </div>
+                        <div class="card-body">
+                            <table id="datatablesSimple" class="table table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>Sno.</th>
+                                        <th>Primeiro Nome</th>
+                                        <th>E-mail</th>
+                                        <?php if ($isAdmin): ?>
+                                            <th>Ação</th>
+                                        <?php endif; ?>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    $ret = mysqli_query($con, "SELECT * FROM users");
+                                    $cnt = 1;
+                                    while ($row = mysqli_fetch_array($ret)) { ?>
+                                        <tr>
+                                            <td><?php echo $cnt; ?></td>
+                                            <td><?php echo htmlspecialchars($row['fname']); ?></td>
+                                            <td><?php echo htmlspecialchars($row['email']); ?></td>
+                                            <?php if ($isAdmin): ?>
+                                                <td>
+                                                    <a href="user-profile.php?uid=<?php echo $row['id']; ?>" class="btn btn-warning btn-sm">
+                                                        <i class="fas fa-edit"></i>
+                                                    </a>
+                                                    <a href="manage-users.php?id=<?php echo $row['id']; ?>" class="btn btn-danger btn-sm" onclick="return confirm('Você realmente deseja excluir?');">
+                                                        <i class="fa fa-trash" aria-hidden="true"></i>
+                                                    </a>
+                                                </td>
+                                            <?php endif; ?>
+                                        </tr>
+                                    <?php $cnt++; } ?>
+                                </tbody>
+                            </table>
+                            <a href="add-user.php" class="btn btn-primary mb-3">Adicionar Novo Usuário</a>
+                        </div>
+                    </div>
+                </div>
+            </main>
+            <?php include('includes/footer.php'); ?>
+        </div>
+    </div>
+</body>
+
+</html>
